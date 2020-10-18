eec8e3ff50862d28e98539eeb7776b42574487b5
aayudeshk <aayudesh.kaparthi@patelhs.org>
2020-10-18 00:25:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    boolean didHomework = true;
+    int covidCases = 3001;
+    if (covidCases < 3000 && didHomework)
+     { 
+       System.out.print("You can go outside");
+     }
+     else 
+    {
+      System.out.print("Dont go outside");
+    }
+  }
+}
